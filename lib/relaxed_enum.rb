9cd51725711c21c9fcf081425dff874723792998
TJ <tj@buildout.com>
2018-10-17 15:43:24 -0400

    Move intger regex to constant

--- lib/relaxed_enum.rb
+++ lib/relaxed_enum.rb
@@ -4,13 +4,14 @@ require 'active_support/core_ext/hash/indifferent_access'
 require 'active_support/inflector'
 
 module RelaxedEnum
+  INTEGER_TEST = /\A[-+]?[0-9]+\z/.freeze
   extend ActiveSupport::Concern
 
   class_methods do
     def relax_enum(attribute)
       define_method("#{attribute}=") do |arg|
         begin
-          if arg.is_a?(String) && !(arg =~ /\A[-+]?[0-9]+\z/).nil?
+          if arg.is_a?(String) && !(arg =~ INTEGER_TEST).nil?
             super(arg.to_i)
           elsif (arg.is_a?(String) || arg.is_a?(Symbol)) && self.class.public_send(attribute.to_s.pluralize).key?(arg)
             super(self.class.public_send(attribute.to_s.pluralize)[arg])
